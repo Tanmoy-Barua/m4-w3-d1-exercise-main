e0f81250fbc3c205897c1c933627c21856b2d818
Tanmoy Barua <43274420+Tanmoy-Barua@users.noreply.github.com>
2024-08-15 19:15:26 -0700

    updated

--- src/components/MyComponent.test.js
+++ src/components/MyComponent.test.js
@@ -3,45 +3,35 @@ import { shallow } from 'enzyme';
 import MyComponent from './MyComponent';
 
 describe("MyComponent", () => {
+  let component;
+  let form;
+
+  beforeEach(() => {
+    component = shallow(<MyComponent />);
+    form = component.find('input');
+  });
+
   it("should render correctly", () => {
-    const component = shallow(<MyComponent />);
+    expect(component).toMatchSnapshot();
   });
 
   it("should render initial layout", () => {
-    // when
-    const component = shallow(<MyComponent />);
-    // then
     expect(component.getElements()).toMatchSnapshot();
   });
 
   it("should create an entry in component state", () => {
-    // given
-    const component = shallow(<MyComponent />);
-    const form = component.find('input');
-  
-    // when
-    form.props().onChange({target: {
-      name: 'myName',
-      value: 'myValue'
-    }});
-  
-    // then
+    // Simulate input change
+    form.simulate('change', { target: { name: 'myName', value: 'myValue' } });
+
+    // Verify the state is defined
     expect(component.state('input')).toBeDefined();
   });
-  
+
   it("should create an entry in component state with the event value", () => {
-    // given
-    const component = shallow(<MyComponent />);
-    const form = component.find('input');
-  
-    // when
-    form.props().onChange({target: {
-      name: 'myName',
-      value: 'myValue'
-    }});
-  
-    // then
+    // Simulate input change
+    form.simulate('change', { target: { name: 'myName', value: 'myValue' } });
+
+    // Verify the state contains the correct value
     expect(component.state('input')).toEqual('myValue');
   });
-
-})
+});
